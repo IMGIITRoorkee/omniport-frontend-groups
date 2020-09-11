0e9d17b7d4f34d3a9359092b05f68332ba91f66c
Sparsh1212 <sparshagrawal1212@gmail.com>
2020-09-11 14:04:53 +0530

    Do formatting as per omniport frontend configuration

--- src/components/group-post-list.js
+++ src/components/group-post-list.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react'
+import { connect } from 'react-redux'
 import {
   Grid,
   Placeholder,
   Segment,
   Icon,
   Card,
-  Button,
-} from "semantic-ui-react";
+  Button
+} from 'semantic-ui-react'
 
-import GroupPostCard from "./group-post-card";
-import GroupAddPost from "./group-add-post";
-import { getTheme } from "formula_one";
+import GroupPostCard from './group-post-card'
+import GroupAddPost from './group-add-post'
+import { getTheme } from 'formula_one'
 
-import "../css/group.css";
+import '../css/group.css'
 
 class GroupPostList extends React.Component {
   loadMore = () => {
-    this.props.loadMore();
-  };
-  render() {
-    const { activeGroupPost, activeGroup } = this.props;
-    const { isLoaded, post } = activeGroupPost;
-    const { results, next } = post;
+    this.props.loadMore()
+  }
+  render () {
+    const { activeGroupPost, activeGroup } = this.props
+    const { isLoaded, post } = activeGroupPost
+    const { results, next } = post
     return (
       <Grid.Column width={11}>
         {activeGroup.hasEditRights && <GroupAddPost />}
         {results &&
-          results.map((post) => {
-            return <GroupPostCard key={post.id} post={post} />;
+          results.map(post => {
+            return <GroupPostCard key={post.id} post={post} />
           })}
         {!isLoaded &&
           [...Array(5)].map((item, index) => {
@@ -36,53 +36,53 @@ class GroupPostList extends React.Component {
               <Card fluid key={index}>
                 <Card.Content>
                   <Placeholder>
-                    <Placeholder.Line length="full" />
+                    <Placeholder.Line length='full' />
                   </Placeholder>
                 </Card.Content>
                 <Card.Content fluid>
-                  <Placeholder style={{ minWidth: "100%" }}>
+                  <Placeholder style={{ minWidth: '100%' }}>
                     <Placeholder.Paragraph>
-                      <Placeholder.Line length="full" />
-                      <Placeholder.Line length="full" />
-                      <Placeholder.Line length="medium" />
+                      <Placeholder.Line length='full' />
+                      <Placeholder.Line length='full' />
+                      <Placeholder.Line length='medium' />
                     </Placeholder.Paragraph>
                     {index % 3 === 2 && <Placeholder.Paragraph />}
                     {index % 3 === 2 && <Placeholder.Image rectangular />}
                   </Placeholder>
                 </Card.Content>
               </Card>
-            );
+            )
           })}
         {isLoaded && !next ? (
-          <Segment basic textAlign="center">
-            <Icon name="frown outline" />
+          <Segment basic textAlign='center'>
+            <Icon name='frown outline' />
             No more bits available. You have scrolled enough for today.
           </Segment>
         ) : (
           <Grid>
-            <Grid.Column textAlign="center">
+            <Grid.Column textAlign='center'>
               <Button animated color={getTheme()} onClick={this.loadMore}>
                 <Button.Content visible>Show more</Button.Content>
                 <Button.Content hidden>
-                  <Icon name="arrow down" />
+                  <Icon name='arrow down' />
                 </Button.Content>
               </Button>
             </Grid.Column>
           </Grid>
         )}
       </Grid.Column>
-    );
+    )
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     activeGroup: state.activeGroup,
-    activeGroupPost: state.activeGroupPost,
-  };
+    activeGroupPost: state.activeGroupPost
+  }
 }
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
+const mapDispatchToProps = dispatch => {
+  return {}
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(GroupPostList);
+export default connect(mapStateToProps, mapDispatchToProps)(GroupPostList)
--- src/components/group-team.js
+++ src/components/group-team.js
@@ -1,81 +1,81 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Container, Menu } from "semantic-ui-react";
+import React from 'react'
+import { connect } from 'react-redux'
+import { Container, Menu } from 'semantic-ui-react'
 
-import CustomBreadcrumb from "core/common/src/components/custom-breadcrumb";
-import GroupTeamList from "./group-team-list";
-import EmptyGroupTeam from "./empty-group-team";
-import { setActiveGroupWithTeam } from "../actions/index";
-import { urlGroupDetailView, urlBaseView } from "../urls";
+import CustomBreadcrumb from 'core/common/src/components/custom-breadcrumb'
+import GroupTeamList from './group-team-list'
+import EmptyGroupTeam from './empty-group-team'
+import { setActiveGroupWithTeam } from '../actions/index'
+import { urlGroupDetailView, urlBaseView } from '../urls'
 
 class GroupTeam extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      activeItem: "current",
-      data: { verboseName: "Current members", activeStatus: true },
-    };
+      activeItem: 'current',
+      data: { verboseName: 'Current members', activeStatus: true }
+    }
   }
-  componentDidMount() {
+  componentDidMount () {
     this.props.SetActiveGroupWithTeam(
       this.props.match.params.slug,
       true,
-      (err) => {
-        this.props.history.push("/404");
+      err => {
+        this.props.history.push('/404')
       }
-    );
+    )
   }
   handleClick = (e, { name }) => {
-    const data = {};
-    if (name === "current") {
-      data["verboseName"] = "Current members";
-      data["activeStatus"] = true;
-      this.props.SetActiveGroupWithTeam(this.props.match.params.slug, true);
+    const data = {}
+    if (name === 'current') {
+      data['verboseName'] = 'Current members'
+      data['activeStatus'] = true
+      this.props.SetActiveGroupWithTeam(this.props.match.params.slug, true)
     }
-    if (name === "alumni") {
-      data["verboseName"] = "Alumni";
-      data["activeStatus"] = false;
-      this.props.SetActiveGroupWithTeam(this.props.match.params.slug, false);
+    if (name === 'alumni') {
+      data['verboseName'] = 'Alumni'
+      data['activeStatus'] = false
+      this.props.SetActiveGroupWithTeam(this.props.match.params.slug, false)
     }
-    this.setState({ activeItem: name, data });
-  };
+    this.setState({ activeItem: name, data })
+  }
 
   loadMore = () => {
-    this.props.handleScroll(true);
-  };
+    this.props.handleScroll(true)
+  }
 
-  render() {
-    const { activeItem } = this.state;
-    const { activeGroup } = this.props;
+  render () {
+    const { activeItem } = this.state
+    const { activeGroup } = this.props
     return (
-      <div style={{ height: "100%" }}>
+      <div style={{ height: '100%' }}>
         <Container>
           <div>
             {activeGroup.data ? (
               <React.Fragment>
                 <CustomBreadcrumb
                   list={[
-                    { name: "Groups", link: urlBaseView() },
+                    { name: 'Groups', link: urlBaseView() },
                     {
                       name: activeGroup.data.name,
                       link: urlGroupDetailView(
-                        activeGroup.data ? activeGroup.data.slug : ""
-                      ),
+                        activeGroup.data ? activeGroup.data.slug : ''
+                      )
                     },
                     {
-                      name: "Team members",
-                    },
+                      name: 'Team members'
+                    }
                   ]}
                 />
                 <Menu tabular>
                   <Menu.Item
-                    name="current"
-                    active={activeItem === "current"}
+                    name='current'
+                    active={activeItem === 'current'}
                     onClick={this.handleClick}
                   />
                   <Menu.Item
-                    name="alumni"
-                    active={activeItem === "alumni"}
+                    name='alumni'
+                    active={activeItem === 'alumni'}
                     onClick={this.handleClick}
                   />
                 </Menu>
@@ -87,22 +87,22 @@ class GroupTeam extends React.Component {
           </div>
         </Container>
       </div>
-    );
+    )
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     activeGroup: state.activeGroup,
-    groupTeam: state.groupTeam,
-  };
+    groupTeam: state.groupTeam
+  }
 }
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     SetActiveGroupWithTeam: (slug, active, errCallback) => {
-      dispatch(setActiveGroupWithTeam(slug, active, errCallback));
-    },
-  };
-};
+      dispatch(setActiveGroupWithTeam(slug, active, errCallback))
+    }
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(GroupTeam);
+export default connect(mapStateToProps, mapDispatchToProps)(GroupTeam)
