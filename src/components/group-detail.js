2fc11fc03b370d4a1c24fc7d0a48c85aaf8a8f21
Sparsh1212 <sparshagrawal1212@gmail.com>
2020-09-11 13:30:46 +0530

    Add load more feature

--- src/components/group-detail.js
+++ src/components/group-detail.js
@@ -1,59 +1,63 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import { connect } from "react-redux";
+import { Container } from "semantic-ui-react";
 
-import CustomBreadcrumb from 'core/common/src/components/custom-breadcrumb'
-import EmptyGroupDetail from './empty-group-detail'
-import GroupBranding from './group-branding'
-import GroupAbout from './group-about'
-import { setActiveGroup } from '../actions'
-import { urlBaseView } from '../urls'
-import '../css/group.css'
+import CustomBreadcrumb from "core/common/src/components/custom-breadcrumb";
+import EmptyGroupDetail from "./empty-group-detail";
+import GroupBranding from "./group-branding";
+import GroupAbout from "./group-about";
+import { setActiveGroup } from "../actions";
+import { urlBaseView } from "../urls";
+import "../css/group.css";
 
 class GroupDetail extends React.Component {
-  componentDidMount () {
-    this.props.SetActiveGroup(this.props.match.params.slug, err => {
-      this.props.history.push('/404')
-    })
+  componentDidMount() {
+    this.props.SetActiveGroup(this.props.match.params.slug, (err) => {
+      this.props.history.push("/404");
+    });
   }
 
-  render () {
-    const { activeGroup } = this.props
+  loadMore = () => {
+    this.props.handleScroll(true);
+  };
+
+  render() {
+    const { activeGroup } = this.props;
     return (
       <div>
         {activeGroup.isLoaded ? (
           <Container>
             <CustomBreadcrumb
               list={[
-                { name: 'Groups', link: urlBaseView() },
-                { name: activeGroup.data.name }
+                { name: "Groups", link: urlBaseView() },
+                { name: activeGroup.data.name },
               ]}
             />
             <GroupBranding />
-            <GroupAbout slug={this.props.match.params.slug} />
+            <GroupAbout
+              loadMore={this.loadMore}
+              slug={this.props.match.params.slug}
+            />
           </Container>
         ) : (
           <EmptyGroupDetail />
         )}
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
-    activeGroup: state.activeGroup
-  }
+    activeGroup: state.activeGroup,
+  };
 }
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     SetActiveGroup: (slug, errCallback) => {
-      dispatch(setActiveGroup(slug, errCallback))
-    }
-  }
-}
+      dispatch(setActiveGroup(slug, errCallback));
+    },
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(GroupDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(GroupDetail);
