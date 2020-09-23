ea1436ea2ef49d0a666d3992422d85f070e6e649
Prernaswarna <prernaswarnakumar@gmail.com>
2020-09-23 16:20:27 +0530

    Change post , about , mission to tinymce fields

--- src/components/group-add-post.js
+++ src/components/group-add-post.js
@@ -186,7 +186,7 @@ class GroupAddPost extends React.Component {
 					'advlist autolink lists link image charmap print preview anchor',
 					'searchreplace visualblocks code fullscreen',
 					'insertdatetime media table paste code help wordcount'
-           		],
+	           		],
 				toolbar:
 					'undo redo | formatselect | bold italic backcolor | \
 				alignleft aligncenter alignright alignjustify | \
