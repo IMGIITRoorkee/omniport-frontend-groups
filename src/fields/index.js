b1b4458ef8d744ac1da752db2263b1c6ef3a446a
Prernaswarna <prernaswarnakumar@gmail.com>
2020-10-05 08:49:29 +0530

    Format changes to index.js in fields

--- src/fields/index.js
+++ src/fields/index.js
@@ -4,36 +4,32 @@ import { TextArea } from 'semantic-ui-react'
 
 import { Editor } from '@tinymce/tinymce-react'
 
-export const RTField = props => {	
-  return (
-    <Editor
-      apikey="fb3pb0ana4mvi60jwhefs3g2u3501d9s915efud2rh6ax2ek"
-      init={{
-        menubar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount'
-          ],
-      }}
-      initalValue="None"
-      value={props.field}
-      textareaName={props.field}
-      onChange={props.handleEditorChange}
-      disabled={props.disabled}
-      inline={props.inline}
-    />
-  )  
-}
+export const RTField = props => (	 
+  <Editor
+    apikey="fb3pb0ana4mvi60jwhefs3g2u3501d9s915efud2rh6ax2ek"
+    init={{
+      menubar: false,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount'
+        ],
+    }}
+    initalValue="None"
+    value={props.field}
+    textareaName={props.field}
+    onChange={props.handleEditorChange}
+    disabled={props.disabled}
+    inline={props.inline}
+  />    
+)
 
-export const Textfield = props => { 
-  return (
-    <TextArea
-      autoHeight
-      name={props.name}
-      value={props.field}
-      onChange={props.handleChange}
-    />
-  )
-}
+export const Textfield = props => ( 
+  <TextArea
+    autoHeight
+    name={props.name}
+    value={props.field}
+    onChange={props.handleChange}
+  />  
+)
 
